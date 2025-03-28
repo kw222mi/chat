0ccb844175e9d85fb45231c1c587dfedc0ad0666
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-03-28 13:44:06 +0100

    add activity check

--- server/public/app.js
+++ server/public/app.js
@@ -1,23 +1,38 @@
-const socket= io('ws://localhost:3500')
+const socket = io("ws://localhost:3500");
 
-function sendMessage (e) {
-    e.preventDefault()
-    const input= document.querySelector('input')
-    if(input.value) {
-        socket.emit('message', input.value)
-        input.value=""
+const activity = document.querySelector(".activity");
+const msgInput = document.querySelector("input");
 
-    }
-    input.focus()
+function sendMessage(e) {
+  e.preventDefault();
+  if (msgInput.value) {
+    socket.emit("message", msgInput.value);
+    msgInput.value = "";
+  }
+  msgInput.focus();
 }
 
-document.querySelector('form')
-.addEventListener('submit', sendMessage)
+document.querySelector("form").addEventListener("submit", sendMessage);
 
-
-//Listen for messages
+// Listen for messages
 socket.on("message", (data) => {
-    const li = document.createElement('li')
-    li.textContent= data
-    document.querySelector('ul').appendChild(li)
-})
+  activity.textContent = "";
+  const li = document.createElement("li");
+  li.textContent = data;
+  document.querySelector("ul").appendChild(li);
+});
+
+msgInput.addEventListener("keypress", () => {
+  socket.emit("activity", socket.id.substring(0, 5));
+});
+
+let activityTimer;
+socket.on("activity", (name) => {
+  activity.textContent = `${name} is typing...`;
+
+  // Clear after 3 seconds
+  clearTimeout(activityTimer);
+  activityTimer = setTimeout(() => {
+    activity.textContent = "";
+  }, 3000);
+});
