590cfbec4592b05405a16e7609bf4766d0dabceb
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-03-28 11:27:26 +0100

    css and klient side

--- server/index.js
+++ server/index.js
@@ -4,15 +4,24 @@ import {Server} from "socket.io"
 const httpServer = createServer()
 
 const io = new Server(httpServer, {
-    cors: {
-        origin: process.env.NODE_ENV === "production" ? false : ["http://localhost:5500"]
-    }
-})
+  cors: {
+    origin:
+      process.env.NODE_ENV === "production"
+        ? false
+        : [
+            "http://localhost:5500",
+            "http://127.0.0.1:5500",
+            "http://127.0.0.1:3500",
+            "http://localhost:3500"
+         
+          ],
+  },
+});
 
 io.on ('connection', socket => {
     console.log(`User ${socket.id} connected`)
 
-    socket.on(message, data => {
+    socket.on('message', data => {
         console.log(data)
         io.emit('message', `${socket.id.substring(0,5)}:  ${data}`)
     })
