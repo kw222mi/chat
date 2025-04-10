417eefc70ffdd1f66844676824859abc11133b9e
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-04-10 16:10:02 +0200

    server adress

--- server/public/app.js
+++ server/public/app.js
@@ -1,4 +1,4 @@
-const socket = io("ws://localhost:3500");
+const socket = io("https://chat-alpha-khaki.vercel.app/");
 
 const msgInput = document.querySelector("#message");
 const nameInput = document.querySelector("#name");
