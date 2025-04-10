6fd416f0c238da8a44f336a15cdfba754fa66a49
Therese Weidenstedt <kw222mi@student.lnu.se>
2025-04-10 16:33:16 +0200

    new cors origin

--- server/index.js
+++ server/index.js
@@ -30,7 +30,11 @@ const io = new Server(expressServer, {
     origin:
       process.env.NODE_ENV === "production"
         ? false
-        : ["http://localhost:5500", "http://127.0.0.1:5500"],
+        : [
+            "http://localhost:5500",
+            "http://127.0.0.1:5500",
+            "https://chat-e3oz.onrender.com",
+          ],
   },
 });
 
